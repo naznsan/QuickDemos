73b47ddcef15bfeb1b35ffcaa454d2a3db6989cf
Justin Kim <naznsan@gmail.com>
2020-09-19 15:02:39 +1200

    Add basic user input to canvas

--- gravityCanvas/App.js
+++ gravityCanvas/App.js
@@ -1,6 +1,17 @@
 const myCanvas = document.getElementById("myCanvas");
+myCanvas.width = 500;
+myCanvas.height = 500;
 
 let context = myCanvas.getContext("2d");
 
-context.font = "30px Arial";
-context.fillText("Hello World!", 10, 50);
+function drawRect(x, y) {
+    context.fillStyle = "green";
+    context.fillRect(x, y, 10, 10);
+}
+
+myCanvas.addEventListener("click", event => {
+    var rect = myCanvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    drawRect(x, y);
+});
---
+++ gravityCanvas/readme.md
@@ -0,0 +1,7 @@
+# gravityCanvas
+A canvas that is affected by gravity, in vanilla JS
+
+## Versions
+### v0.1
+Added very basic user input to draw small green squares based on input
+![v0.1 screenshot](readme-resources/v01.png)
