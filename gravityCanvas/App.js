ada74285d4fe53e66baa8d762f27bd3ccb1f0143
Justin Kim <naznsan@gmail.com>
2020-09-20 02:35:08 +1200

    Add circles and buttons to change shape

--- gravityCanvas/App.js
+++ gravityCanvas/App.js
@@ -1,6 +1,9 @@
 const myCanvas = document.getElementById("myCanvas");
+const rectBtn = document.getElementById("rectBtn");
+const circleBtn = document.getElementById("circleBtn");
 let context = myCanvas.getContext("2d");
 let shapes = [];
+let selectedShape = "rect";
 
 function getColor() {
     const colors = ["#f79256", "#fbd1a2", "#7dcfb6", "#00b2ca", "#1d4e89"];
@@ -17,10 +20,10 @@ function Shape(shape, posX, posY, color, falling) {
 
 function gravitate(shape) {
     const gravity = 5;
-    if (shape.y >= myCanvas.height - 30) {
+    if (shape.y >= myCanvas.height - 15) {
         shape.falling = false;
-    } else if (shape.y + gravity > myCanvas.height - 30) {
-        shape.y += (myCanvas.height - 30 - shape.y);
+    } else if (shape.y + gravity > myCanvas.height - 15) {
+        shape.y += (myCanvas.height - 15 - shape.y);
     } else {
         shape.y += gravity;
     }
@@ -29,7 +32,11 @@ function gravitate(shape) {
 function drawShape(shape) {
     context.fillStyle = shape.color;
     if (shape.shape == "rect") {
-        context.fillRect(shape.x, shape.y, 30, 30);
+        context.fillRect(shape.x - 15, shape.y - 15, 30, 30);
+    } else if (shape.shape == "circle") {
+        context.beginPath();
+        context.arc(shape.x, shape.y, 15, 0, 2 * Math.PI);
+        context.fill();
     }
     if (shape.falling) {
         gravitate(shape);
@@ -42,11 +49,26 @@ function loop() {
     requestAnimationFrame(loop);
 }
 
+rectBtn.disabled = true;
+circleBtn.disabled = false;
+
 myCanvas.addEventListener("click", event => {
     var rect = myCanvas.getBoundingClientRect();
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
-    shapes.push(new Shape("rect", x, y, getColor()));
+    shapes.push(new Shape(selectedShape, x, y, getColor()));
+});
+
+rectBtn.addEventListener("click", event => {
+    selectedShape = "rect";
+    rectBtn.disabled = true;
+    circleBtn.disabled = false;
+});
+
+circleBtn.addEventListener("click", event => {
+    selectedShape = "circle";
+    rectBtn.disabled = false;
+    circleBtn.disabled = true;
 });
 
 requestAnimationFrame(loop);
--- gravityCanvas/readme.md
+++ gravityCanvas/readme.md
@@ -1,9 +1,12 @@
 # gravityCanvas
-A canvas that is affected by gravity, in vanilla JS
+A canvas that is affected by gravity, in vanilla JS with no libraries or frameworks
 
 Download files and open `index.html` in a browser
 
 ## Versions
+### v0.3
+Added circles and buttons to change shape
+![v0.3 screenshot](readme-resources/v03.gif)
 ### v0.2
 Added constant gravity
 ![v0.2 screenshot](readme-resources/v02.gif)
